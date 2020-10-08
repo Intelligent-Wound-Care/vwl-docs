9817ae3bf1a598f09cf20b07cf1854ac5855414d
Milo Halsey-Perry <milo.halseyperry@gmail.com>
2020-10-08 19:51:53 +0100

    inital commit and setup of some md pages.

---
+++ sidebars.js
@@ -0,0 +1,17 @@
+module.exports = {
+  someSidebar: {
+    Docusaurus: ['doc1', 'doc2', 'doc3'],
+    Features: ['mdx'],
+    Resources: [
+      'articles',
+      'content-themes',
+      'events',
+      'people',
+      'person-types',
+      'tags',
+      'users',
+      'videos',
+      'webinars',
+    ],
+  },
+};
